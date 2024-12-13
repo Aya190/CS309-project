f0f9b42ba046da7b3c1e1e51288752a978bc2b49
Aya190 <121063569+Aya190@users.noreply.github.com>
2024-12-13 23:19:05 +0200

    creat usercontroller

---
+++ backend/db/user.js
@@ -0,0 +1,36 @@
+
+const User = require('../db/models/user');
+
+const createUser = async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        imagUrl: req.body.imagUrl
+
+    });
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: err.message });
+    }
+};
+const getUsurByEmail = async (req,res) => {
+   try {
+    const email = await User.find();
+    res.json(email);
+   } catch (error) {
+    res.status(500).json({ message: err.message });
+
+   }
+};
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: err.message });
+    }
+};
