e064c08ca3d4b4ab8214ad8532ff0817ccb659ab
Aya190 <121063569+Aya190@users.noreply.github.com>
2024-12-13 23:35:10 +0200

    updet in user

--- backend/db/user.js
+++ backend/db/user.js
@@ -34,3 +34,10 @@ const getAllUsers = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+module.exports = {
+    createUser, 
+    getUsurByEmail,
+    getAllUsers
+   
+};
