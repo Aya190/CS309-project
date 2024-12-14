d053212389e4308ff16f9263bbe12e625aa0f965
Aya Abdelmuniem <>
2024-12-14 02:00:16 +0200

    createOrder and UpdateCratitem

--- backend/db/models/cartitem.js
+++ backend/db/models/cartitem.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const CartSchema = new mongoose.Schema({
+const CartSchema = new Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }],
     quantity: { type: Number, required: true },
