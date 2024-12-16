78517e2b78a2a004934b4a5962d6f8e22525304d
yasso1912 <yasmin.osama192002@gmail.com>
2024-12-17 00:27:36 +0200

    update app

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,15 @@
-const express = require("express")
-const mongoose = require('mongoose')
-const User = require ('./db/schema/user')
+const express = require("express");
+const mongoose = require('mongoose');
+const User = require ('./db/models/user');
+const cors = require("cors");
 
 
+const products = require("./router/product/product");
+const { router: upload } = require("./router/upload/upload");
+const app = express();
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/product", product);
+app.use("/upload", upload);
