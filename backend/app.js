a335fb06dd37ea40d8b5ce71c5da7d082bb0437b
Aya190 <121063569+Aya190@users.noreply.github.com>
2024-12-17 00:38:17 +0200

    update app

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const mongoose = require('mongoose');
-const User = require ('./db/models/user');
 const cors = require("cors");
-
-
 const products = require("./router/product/product");
+const { router: user } = require("./router/user/user");
 const { router: upload } = require("./router/upload/upload");
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 
-app.use("/product", product);
+mongoose.set("strictQuery", false)
+mongoose
+.connect("mongodb://localhost:27017/CS309-project")
+.then(() => {
+    console.log('connected to MongoDB')
+    
+    app.listen(3000, () => console.log('app started on port 3000'))
+}).catch((error) => {
+    console.log('cant connect to mongodb'+error)
+});
+
+
+app.use("/product", products);
+app.use("/user" , user);
 app.use("/upload", upload);
