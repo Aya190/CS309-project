4bce7c05896a0d577f49b616df97781abc6714e3
FatmaEl-zahraaali <fatmaali8053@gmail.com>
2024-12-20 12:34:54 +0200

    /
    create home,login,signup,router for them

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,34 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import Layout from './pages/home/navbar/Navbar'
+import Signup from'./pages/login/signup'
+import Home from './pages/home/home'
+import Login from './pages/login/login'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      
+      
+      
+          
+    </Route>
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<Signup />} />
+    
+    
+    </Routes>
+</BrowserRouter>
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
