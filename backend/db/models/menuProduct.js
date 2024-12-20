7553594009630093a9a9e8cf40172dc2ddf3ff61
yasso1912 <yasmin.osama192002@gmail.com>
2024-12-20 23:30:43 +0200

    update menuProduct

--- backend/db/models/menuProduct.js
+++ backend/db/models/menuProduct.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const MenuProductSchema = new Schema({
     name: { type: String, required: true },
-    description: { type: String, required: true },
     price: { type: Number, required: true },
     available: { type: Boolean, default: true },
     imagUrl:{ type:String,required: true}
