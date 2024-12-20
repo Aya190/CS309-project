30a8c8aa2fe932805bcd08b6ebc8552cdaeab4e7
alaasayedsadek <167907522+alaasayedsadek@users.noreply.github.com>
2024-12-20 15:52:39 +0200

    Route pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ItemList from './Pages/ItemList';
 import ItemDetails from './Pages/ItemDetails';
+import Login from './Pages/login/login';
+import Signup from './Pages/login/signup';
+import Home from './Pages/home/Home';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ItemList />} /> 
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} /> 
+      <Route path="/sign" element={<Signup/>} /> 
+        <Route path="/itemlist" element={<ItemList />} /> 
         <Route path="/item/:id" element={<ItemDetails />} />
       </Routes>
     </Router>
