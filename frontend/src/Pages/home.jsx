eab58bb53a575a17c0765eb4efa45af187bb895e
FatmaEl-zahraaali <fatmaali8053@gmail.com>
2024-12-20 11:18:24 +0200

    Add files via upload

---
+++ frontend/src/Pages/home.jsx
@@ -0,0 +1,11 @@
+import './home.css'
+const Home = () => {
+    return (
+    <div className="Home">
+        <h1>Your Table Awaits!</h1>
+   <h3>Welcome to our Resturant </h3><h3> Where every meal is a celebration </h3><h3>Join us for a culinary adventure.</h3>
+
+    </div>)
+  };
+  
+  export default Home;
