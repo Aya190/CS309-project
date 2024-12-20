0a2864d404702fd1f26dfffa035d802127bb3d3b
alaasayedsadek <167907522+alaasayedsadek@users.noreply.github.com>
2024-12-20 02:28:02 +0200

    change search

--- frontend/src/Pages/SearchBar.js
+++ frontend/src/Pages/SearchBar.js
@@ -1,20 +1,20 @@
-// SearchBar.js
+
 import React, { useState } from 'react';
 
 const SearchBar = ({ data, setFilteredItems }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  // عندما يتغير النص في الـ Search Bar، يتم تحديث العناصر المفلترة
+  
   const handleSearch = (e) => {
     const term = e.target.value;
     setSearchTerm(term);
 
-    // فلترة البيانات بناءً على تطابق الـ title مع النص المدخل
+    
     const filteredData = data.filter((item) =>
       item.title.toLowerCase().includes(term.toLowerCase())
     );
     
-    // إرسال العناصر المفلترة إلى الـ Parent component (ItemList)
+    
     setFilteredItems(filteredData);
   };
 
