fee0cf0385fbfbff6f8de69b936d6541ad3abf62
FatmaEl-zahraaali <fatmaali8053@gmail.com>
2024-12-21 02:04:26 +0200

    Update home.jsx

--- frontend/src/Pages/home/home.jsx
+++ frontend/src/Pages/home/home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
     <div className="Home">
       <Layout/>
-        <h1>Your Table Awaits!</h1>
+        <h1>FoodLover Resturant</h1>
    <h3>Welcome to our Resturant </h3><h3> Where every meal is a celebration </h3><h3>Join us for a culinary adventure.</h3>
    <Link to="/itemlist">
         <button>Go To Item List</button>
